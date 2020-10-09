64648bd4422825f17e3d854d7ab1d0dea315cc13
Iain Shorter <iain.shorter@nanoporetech.com>
2020-10-09 14:40:36 +0100

    add root exports

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+export * from './type-cast/as-json';
+export * from './type-cast/as-primitive';
+export * from './type-check/is-json';
+export * from './type-check/is-primitive';
+export * from './type-coerce/make-primative';
+
+export { Index, Indexable } from './Index.type';
+export { JSONValue, JSONArray, JSONObject } from './JSONValue.type';
+export { ObjectDict } from './ObjectDict.type';
+export { UnknownFunction, UnknownAsyncFunction } from './UnknownFunction.type';
