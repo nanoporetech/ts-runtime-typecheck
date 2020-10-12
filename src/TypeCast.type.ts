cc9ce2d532f4b3cc5602b83fda2fbf6906452add
Iain Shorter <iain.shorter@nanoporetech.com>
2020-10-12 12:19:28 +0100

    add: introduce new Optional and OptionalTypeCast types

---
+++ src/TypeCast.type.ts
@@ -0,0 +1,4 @@
+import type { Optional } from './Optional.type';
+
+export type TypeCast<Output, Input = unknown> = (value: Input, fallback?: Output) => Output;
+export type OptionalTypeCast<Output, Input = unknown> = (value: Input) => Optional<Output>;
