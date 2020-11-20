2b588827f5ac388f8b7969d925603441c8ec0f12
Iain Shorter <iain.shorter@nanoporetech.com>
2020-11-20 16:22:33 +0000

    tidy: remove invalid commment from InterfacePattern

--- src/InterfacePattern.type.ts
+++ src/InterfacePattern.type.ts
@@ -2,5 +2,4 @@ import type { Dictionary } from './Dictionary.type';
 import type { TypeAssert, UnwrapTypeAssert } from './TypeAssert.type';
 
 export type InterfacePattern = Dictionary<TypeAssert<unknown>>;
-// NOTE while the below does work, we are not currently using it as over-complicates the visible return type ( while still be functionally identical )
 export type UnwrapInterfacePattern<Pattern extends InterfacePattern> = { [K in keyof Pattern]: UnwrapTypeAssert<Pattern[K]>};
