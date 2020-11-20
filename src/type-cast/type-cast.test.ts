c86dbb00881d8533e8d37a136162d7487c6c08cf
Iain Shorter <iain.shorter@nanoporetech.com>
2020-11-20 12:17:58 +0000

    test: update type-cast tests to use new format for optTypeCast

--- src/type-cast/type-cast.test.ts
+++ src/type-cast/type-cast.test.ts
@@ -25,16 +25,16 @@ describe('typeCast', () => {
 
 describe('optTypeCast', () => {
   const passing_test_fn = (_obj: unknown): _obj is unknown => true;
-  const passing_cast_fn = optTypeCast(typeCast('PassingTest', passing_test_fn));
+  const passing_cast_fn = optTypeCast('PassingTest', passing_test_fn);
   const failing_test_fn = (_obj: unknown): _obj is unknown => false;
-  const failing_cast_fn = optTypeCast(typeCast('FailingTest', failing_test_fn));
+  const failing_cast_fn = optTypeCast('FailingTest', failing_test_fn);
 
   it('emits value with passing test', () => {
     expect(passing_cast_fn(12)).toBe(12);
   });
 
   it('throws with failing test', () => {
-    expect(() => failing_cast_fn(12)).toThrow('Unable to cast number to FailingTest');
+    expect(() => failing_cast_fn(12)).toThrow('Unable to cast number to Optional<FailingTest>');
   });
 
   it('emits for nullish value with failing test', () => {
