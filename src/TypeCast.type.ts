3aa3058843d0c8c9e0865ff49b43e3fcb93cd047
Iain Shorter <iain.shorter@nanoporetech.com>
2020-11-20 12:23:53 +0000

    change: add Optional to input param of the signature for OptionalTypeCast, to ensure explicit definitions include Optional on input

--- src/TypeCast.type.ts
+++ src/TypeCast.type.ts
@@ -1,2 +1,4 @@
+import type { Optional } from './Optional.type';
+
 export type TypeCast<Output, Input = unknown> = (value: Input, fallback?: Output) => Output;
-export type OptionalTypeCast<Output, Input = unknown> = (value: Input) => Output | undefined;
+export type OptionalTypeCast<Output, Input = unknown> = (value: Optional<Input>) => Output | undefined;
