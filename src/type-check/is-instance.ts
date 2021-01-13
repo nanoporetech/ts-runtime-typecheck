8642901cc3e2fd41836d933f77f10ad36ed77548
Iain Shorter <iain.shorter@nanoporetech.com>
2021-01-13 18:28:14 +0000

    delete: isInstance ( experimental, to be introduced in another MR )

--- src/type-check/is-instance.ts
+++
@@ -1,7 +0,0 @@
-import type { TypeAssert } from '../TypeAssert.type';
-
-export function isInstance<T extends new (...args: any[]) => unknown>(ctor: T): TypeAssert<InstanceType<T>> {
-  return (value: unknown): value is InstanceType<T> => {
-    return value instanceof ctor;
-  };
-}
