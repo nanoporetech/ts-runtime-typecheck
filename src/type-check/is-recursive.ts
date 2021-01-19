db706dbdf5265882498e1bcc6656be40a6cdbd37
Iain Shorter <iain.shorter@nanoporetech.com>
2021-01-19 10:45:56 +0000

    fix: incorrect return type on isRecordRecursive|isOptRecordRecursive

--- src/type-check/is-recursive.ts
+++ src/type-check/is-recursive.ts
@@ -1,19 +1,20 @@
 import type { Optional } from '../Optional.type';
 import type { TypeAssert } from '../TypeAssert.type';
+import type { Dictionary } from '../Dictionary.type';
 
 import { memoize } from '../memoize';
 import { isArray, isNullish, isRecord } from './is-primitive';
 
-export const isRecordRecursive = memoize(<T> (isType: TypeAssert<T>): TypeAssert<T[]> => {
-  const result = (obj: unknown): obj is T[] => {
+export const isRecordRecursive = memoize(<T> (isType: TypeAssert<T>): TypeAssert<Dictionary<T>> => {
+  const result = (obj: unknown): obj is Dictionary<T> => {
     return isRecord(obj) && Object.values(obj).every(isType);
   };
   result.TYPE_NAME = `Dictionary<${isType.TYPE_NAME ?? 'unknown'}>`;
   return result;
 });
 
-export const isOptRecordRecursive = memoize(<T> (isType: TypeAssert<T>): TypeAssert<Optional<T[]>> => {
-  const result = (obj: unknown): obj is T[] => {
+export const isOptRecordRecursive = memoize(<T> (isType: TypeAssert<T>): TypeAssert<Optional<Dictionary<T>>> => {
+  const result = (obj: unknown): obj is Dictionary<T> => {
     return isNullish(obj) || isRecord(obj) && Object.values(obj).every(isType);
   };
   result.TYPE_NAME = `Optional<Dictionary<${isType.TYPE_NAME ?? 'unknown'}>>`;
