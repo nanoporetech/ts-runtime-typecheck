f4da6c41cfdd33afdd379d39a6d91580f45437b9
Iain Shorter <iain.shorter@nanoporetech.com>
2021-01-19 17:26:47 +0000

    change: update memoize so that it will accept anything that WeakMap will

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,7 +1,5 @@
-import type { Dictionary } from './Dictionary.type';
-import type { UnknownFunction } from './UnknownFunction.type';
-
-export function memoize<P extends (UnknownFunction | Dictionary), R> (fn: (par: P) => R): (par: P) => R {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function memoize<P extends object, R> (fn: (par: P) => R): (par: P) => R {
   const map: WeakMap<P, R> = new WeakMap();
   return (par: P) => {
     let result = map.get(par);
