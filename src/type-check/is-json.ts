a131778c255fce8670c77fc1ad4cc40192e62e53
Iain Shorter <iain.shorter@nanoporetech.com>
2021-10-04 13:56:37 +0100

    update deps and linting config

--- src/type-check/is-json.ts
+++ src/type-check/is-json.ts
@@ -6,19 +6,19 @@ import { isNullish } from './is-primitive';
 export function isJSONValue(val: unknown): val is JSONValue {
   // WARN this is recursive and will blow the stack on a circular object
   switch (typeof val) {
-  case 'string':
-  case 'number':
-  case 'boolean':
-    return true;
-  case 'object': {
-    if (val === null) {
+    case 'string':
+    case 'number':
+    case 'boolean':
       return true;
+    case 'object': {
+      if (val === null) {
+        return true;
+      }
+      if (Array.isArray(val)) {
+        return val.every(isJSONValue);
+      }
+      return Object.values(val).every(isJSONValue);
     }
-    if (Array.isArray(val)) {
-      return val.every(isJSONValue);
-    }
-    return Object.values(val).every(isJSONValue);
-  }
   }
   return false;
 }
