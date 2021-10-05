7a7efd8cd55baea0b1d3df45a35dccff417e6e5b
Iain Shorter <iain.shorter@nanoporetech.com>
2021-10-05 17:41:14 +0100

    correct type of asOptLiteral

--- src/type-cast/as-literal.ts
+++ src/type-cast/as-literal.ts
@@ -1,13 +1,12 @@
-import type { Optional } from '../Optional.type';
 import type { Primitive } from '../Primitive.type';
 import { isLiteral } from '../type-check/is-literal';
-import type { TypeCast } from '../TypeCast.type';
+import type { OptionalTypeCast, TypeCast } from '../TypeCast.type';
 import { optTypeCast, typeCast } from './type-cast';
 
 export function asLiteral<T extends Primitive>(literal: T): TypeCast<T> {
   return typeCast(isLiteral(literal));
 }
 
-export function asOptLiteral<T extends Primitive>(literal: T): TypeCast<Optional<T>> {
+export function asOptLiteral<T extends Primitive>(literal: T): OptionalTypeCast<T> {
   return optTypeCast(isLiteral(literal));
 }
